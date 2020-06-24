adc133ab221ba85f2b589b9389ee82174b1d3ca9
Hannah Silver <hannah_silver@hotmail.com>
2020-06-24 14:54:09 -0400

    exercise 2

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,10 +1,57 @@
 // Preset values
 const FROGS = 3;
-
+const track = document.getElementById("track");
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
 
-// 2. Create li
+for (let laneNumber = 1; laneNumber <= FROGS; laneNumber++) {
+  let lane = document.createElement("li");
+  track.appendChild(lane);
 
-// 3. Create span and add it to the li
+  const span = document.createElement("span");
 
-// 4. Assign an id to each lane
+  span.innerText = laneNumber;
+  lane.appendChild(span);
+
+  lane.id = "frog" + laneNumber;
+}
+
+let racers = [];
+
+for (let i = 0; i < FROGS; i++) {
+  const froggy = frogStable[i];
+  racers.push(froggy);
+}
+
+console.log(racers);
+
+for (let i = 0; i < racers.length; i++) {
+  const lane = document.getElementById(`frog${i + 1}`);
+  const span = document.createElement("span");
+  span.innerText = racers[i].name;
+  lane.appendChild(span);
+  span.className = "frog";
+  span.style.backgroundColor = racers[i].color;
+  racers[i].progress = 0;
+  racers[i].lane = i + 1;
+  span.id = i + 1;
+}
+
+function racingFrog(frogObject) {
+  const random = Math.random() * 100;
+  const interval = setInterval(function () {
+    frogObject.progress += random;
+    const frogElement = document.getElementById(frogObject.lane);
+    console.log(frogElement);
+    if (frogObject.progress > 100) {
+      frogElement.style.left = "100%";
+      clearInterval(interval);
+      console.log(frogObject);
+      return;
+    }
+    frogElement.style.left = `${frogObject.progress}%`;
+  }, Math.random() * 1000);
+}
+
+for (let i = 0; i < racers.length; i++) {
+  racingFrog(racers[i]);
+}
